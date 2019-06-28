a52e7566841c99c8a6eb3834fed158d2ed126ff2
Evgeny Mikhaylov <evgenmikhaylov@gmail.com>
2019-06-28 10:54:45 +0600

    Update gitignore

--- Example/Carthage/Checkouts/DeepDiff/Example/DeepDiffDemo/DeepDiffDemo/Sources/DataSet.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct DataSet {
-  static func generateItems() -> [Int] {
-    let count = Int(arc4random_uniform(20)) + 10
-    let items = Array(0..<count)
-    return items.shuffled()
-  }
-}
