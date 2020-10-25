79dbbb9e3c0d7207b4008495b7cf27b4b540d181
Nik <nikolay.tyunin@rosberry.com>
2020-10-25 17:05:14 +0600

    implement UniversalCollectionViewCellItem associated object

--- Sources/Items/UniversalCollectionViewCellItem.swift
+++ Sources/Items/UniversalCollectionViewCellItem.swift
@@ -6,11 +6,15 @@
 
 import UIKit
 
-public final class UniversalCollectionViewCellItem<T: UICollectionViewCell>: CollectionViewDiffCellItem {
+public final class UniversalCollectionViewCellItem<U: GenericDiffItem, T: UICollectionViewCell>: CollectionViewDiffCellItem {
 
-    public let reuseType = ReuseType.class(T.self)
-    public var context = [String: Any]()
-    public lazy var diffIdentifier: String = .init(describing: self)
+    public lazy var reuseType = ReuseType.class(T.self)
+    public lazy var diffIdentifier: String = "\(String(describing: type(of: self))){\(object.diffIdentifier)}"
+    public let object: U
+
+    init(object: U) {
+        self.object = object
+    }
     
     /// Set this handler to configure the cell
     ///
@@ -24,12 +28,6 @@ public final class UniversalCollectionViewCellItem<T: UICollectionViewCell>: Col
     ///    - UICollectionView: collection view where cell should be placed
     ///    - CollectionViewSectionItem: a section item in the section of which the cell should be placed
     public var sizeConfigurationHandler: ((UICollectionView, CollectionViewSectionItem) -> CGSize)?
-
-    /// Set this handler to compare cellItems
-    ///
-    /// - Parameters:
-    ///    - `UniversalCollectionViewCellItem<T>`: cellItem that should be compared with `self`
-    public var isEqualHandler: ((UniversalCollectionViewCellItem<T>) -> Bool)?
     
     public func configure(_ cell: UICollectionViewCell) {
         guard let cell = cell as? T else {
@@ -43,9 +41,9 @@ public final class UniversalCollectionViewCellItem<T: UICollectionViewCell>: Col
     }
 
     public func isEqual(to item: DiffItem) -> Bool {
-        guard let cellItem = item as? UniversalCollectionViewCellItem<T> else {
+        guard let cellItem = item as? UniversalCollectionViewCellItem<U, T> else {
             return false
         }
-        return isEqualHandler?(cellItem) ?? true
+        return object.isEqual(to: cellItem)
     }
 }
